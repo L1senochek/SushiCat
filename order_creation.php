f85e5ea9929487315f780680698b7113a1621afb
Tatiana <106532861+L1senochek@users.noreply.github.com>
2022-06-28 11:56:51 +0300

    Update order_creation.php

--- order_creation.php
+++ order_creation.php
@@ -1,20 +1,13 @@
 <?php
 session_start();
 include "header.php";
-
 include"data_base.php";
 $style = "resource/CSS/header.css";
-
 ?>
 <div class="bgc_order">
 <div class="order"> Ваш заказ: </div> 
-
 <div class="break"></div>
-
-
-
 <?php 
-
   $cart_list = $_SESSION['cart_list']; //массив из сессии
   if(empty($cart_list)){
     echo '<div class="cart_empty"> Корзина пустая <br>
@@ -28,7 +21,6 @@ $style = "resource/CSS/header.css";
   while( $dishes = $result->fetch_assoc() ):
     ?>
   
-  
   <!--<div class="order_creat_cart">-->
     
     <div class="order_creat_cart_main">
@@ -40,9 +32,7 @@ $style = "resource/CSS/header.css";
       </div> 
       <div class="cart_dishes_price">
         <p>цена: <?=$dishes['price']?> руб x <?php echo $cart_list[$dishes['id']] ?> шт</p>
-      </div>
-      
-      
+      </div> 
     </div>
     <?php
     $sum = $sum + ($cart_list[ $dishes['id'] ])*$dishes['price'];
@@ -50,23 +40,16 @@ $style = "resource/CSS/header.css";
     endwhile;
      
     //print_r($order_composition); 
-    
     $order_composition = substr($order_composition,0,-2);
    // print_r($order_composition)
-  
-  ?>
+    ?>
   <div class="total_sum">
     Общая сумма заказа: <?php echo $sum ?> руб
-    
-    <br>
-    
+    <br> 
   </div>
   <?php 
-  
-  
   ?>
-  
-  
+ 
  <!-- </div>-->
   <div class="personal_data">
     <form action="order_creation_obr.php" method="post" class="personal_data_form">
@@ -115,15 +98,11 @@ $style = "resource/CSS/header.css";
     </form>
     
   </div>
-  
- 
   <div class="href">
     <button class="href_index_btn"><a class="href_index" href="index.php"> Продолжить покупки</a> </button><br>
     <button class="href_cart_btn"><a class="href_cart" href="cart.php"> Вернуться в коризу</a> </button><br>
-  </div>
-  
+  </div> 
   <?php
-
   ?>
   </div>
 <?php
